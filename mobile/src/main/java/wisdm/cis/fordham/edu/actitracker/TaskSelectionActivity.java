603d54f8ec8207fa7f0ef68fecb896a872be1038
Jessica Timko <jtimko1@fordham.edu>
2016-06-01 16:09:04 -0400

    Implemented adding of tasks by the user

--- mobile/src/main/java/wisdm/cis/fordham/edu/actitracker/TaskSelectionActivity.java
+++ mobile/src/main/java/wisdm/cis/fordham/edu/actitracker/TaskSelectionActivity.java
@@ -1,11 +1,23 @@
 package wisdm.cis.fordham.edu.actitracker;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Activity where user can add/choose task to log sensor data for.
@@ -15,6 +27,9 @@ public class TaskSelectionActivity extends AppCompatActivity {
     private static final String TAG = "TaskSelectionActivity";
 
     private String username;
+    ArrayList<String> taskList = new ArrayList<>();
+    ArrayAdapter<String> mAdapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,21 @@ public class TaskSelectionActivity extends AppCompatActivity {
 
         Intent i = getIntent();
         username = i.getStringExtra("USERNAME");
+
+        listView = (ListView) findViewById(R.id.list);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, taskList);
+        listView.setAdapter(mAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String activity = listView.getItemAtPosition(i).toString();
+                Intent intent = new Intent(getApplicationContext(), SensorLogActivity.class);
+                intent.putExtra("USERNAME", (getUsername()));
+                intent.putExtra("ACTIVITY_NAME", activity);
+                startActivity(intent);
+            }
+        });
     }
 
     public String getUsername() {
@@ -46,5 +76,40 @@ public class TaskSelectionActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void addActivity(View view) {
+        final EditText input = new EditText(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        input.setHint("Task name");
+        input.setLayoutParams(lp);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New task");
+        builder.setMessage("Enter new task name");
+        builder.setView(input);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                taskList.add(input.getText().toString());
+                mAdapter.notifyDataSetChanged();
+
+                Intent intent = new Intent(getApplicationContext(), SensorLogActivity.class);
+                intent.putExtra("USERNAME", (getUsername()));
+                intent.putExtra("ACTIVITY_NAME", input.getText().toString());
+                startActivity(intent);
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.create().show();
+
     }
 }
